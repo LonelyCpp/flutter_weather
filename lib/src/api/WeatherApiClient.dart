8e1d3c4704930b2b80d7f09b854d09043e4cfe3c
Ananthu Kanive <ananthu.kanive@ymedialabs.com>
2019-05-20 15:16:50 +0530

    fix git ignore add weather api client

---
+++ lib/src/api/WeatherApiClient.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'package:flutter_weather/src/model/Weather.dart';
+import 'package:meta/meta.dart';
+import 'package:http/http.dart' as http;
+
+class WeatherApiClient {
+  static const baseUrl = 'http://api.openweathermap.org';
+  final apiKey;
+  final http.Client httpClient;
+
+  WeatherApiClient({
+    @required this.httpClient,
+    this.apiKey
+  }) : assert(httpClient != null);
+
+  Future<Weather> getWeatherData(String cityName) async {
+    final url = '$baseUrl/data/2.5/weather?q=$cityName&appid=$apiKey';
+
+    final res = await this.httpClient.get(url);
+    if (res.statusCode != 200) {
+      throw Exception('error getting locationId for city');
+    }
+    final weatherJson = json.decode(res.body);
+    return Weather.fromJson(weatherJson['weather'][0]);
+  }
+}
