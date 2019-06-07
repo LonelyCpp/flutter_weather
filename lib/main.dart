dc20b3cb8f46d6500fd8d6fe520adfa6bddb865e
Ananthu Kanive <ananthu.kanive@ymedialabs.com>
2019-06-07 18:09:21 +0530

    add an inherited widget in root widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,7 @@ import 'package:flutter_weather/src/themes.dart';
 
 void main() {
   BlocSupervisor().delegate = SimpleBlocDelegate();
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Weather App',
-      theme: Themes.dark,
-      home: WeatherScreen(),
-    );
-  }
+  runApp(RootWidget());
 }
 
 class SimpleBlocDelegate extends BlocDelegate {
@@ -24,5 +13,80 @@ class SimpleBlocDelegate extends BlocDelegate {
   onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
     print(transition);
+  }
+}
+
+class RootWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppStateContainer(child: WeatherApp());
+  }
+}
+
+class WeatherApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppStateContainer(
+        child: MaterialApp(
+      title: 'Flutter Weather App',
+      theme: AppStateContainer.of(context).theme,
+      home: WeatherScreen(),
+    ));
+  }
+}
+
+class AppStateContainer extends StatefulWidget {
+  final Widget child;
+
+  AppStateContainer({@required this.child});
+
+  @override
+  _AppStateContainerState createState() => _AppStateContainerState();
+
+  static _AppStateContainerState of(BuildContext context) {
+    return (context.inheritFromWidgetOfExactType(_InheritedStateContainer)
+            as _InheritedStateContainer)
+        .data;
+  }
+}
+
+class _AppStateContainerState extends State<AppStateContainer> {
+  ThemeData _theme = Themes.light;
+
+  @override
+  Widget build(BuildContext context) {
+    print(theme.accentColor);
+    return _InheritedStateContainer(
+      data: this,
+      child: widget.child,
+    );
+  }
+
+  ThemeData get theme => _theme;
+
+  updateTheme(ThemeData value) {
+    setState(() {
+      _theme = value;
+      Theme.of(context).copyWith(
+        primaryColor: value.primaryColor,
+        accentColor: value.accentColor,
+      );
+    });
+  }
+}
+
+class _InheritedStateContainer extends InheritedWidget {
+  final _AppStateContainerState data;
+
+  const _InheritedStateContainer({
+    Key key,
+    @required this.data,
+    @required Widget child,
+  }) : super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(_InheritedStateContainer oldWidget) {
+    print('updateShouldNotify');
+    return true;
   }
 }
