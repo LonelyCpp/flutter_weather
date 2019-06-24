463fe8e8d5f2d7485cd63a675890bea55d0b7422
LonelyCpp <ananthu.kanive6@gmail.com>
2019-06-24 15:33:01 +0530

    add location based weather fetch

--- lib/src/api/weather_api_client.dart
+++ lib/src/api/weather_api_client.dart
@@ -13,6 +13,19 @@ class WeatherApiClient {
       : assert(httpClient != null),
         assert(apiKey != null);
 
+  Future<String> getCityNameFromLocation(
+      {double latitude, double longitude}) async {
+    final url =
+        '$baseUrl/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey';
+    print('fetching $url');
+    final res = await this.httpClient.get(url);
+    if (res.statusCode != 200) {
+      throw HTTPException(res.statusCode, "unable to fetch weather data");
+    }
+    final weatherJson = json.decode(res.body);
+    return weatherJson['name'];
+  }
+
   Future<Weather> getWeatherData(String cityName) async {
     final url = '$baseUrl/data/2.5/weather?q=$cityName&appid=$apiKey';
     print('fetching $url');
