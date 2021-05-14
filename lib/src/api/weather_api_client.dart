d98cbee41b49be8079cfaf6fdf795d1fb273270c
Ananthu Kanive <ananthu.kanive@smallcase.com>
2021-05-14 19:00:55 +0530

    update http client to use constructed URI

--- lib/src/api/weather_api_client.dart
+++ lib/src/api/weather_api_client.dart
@@ -15,39 +15,63 @@ class WeatherApiClient {
       : assert(httpClient != null),
         assert(apiKey != null);
 
+  Uri _buildUri(String endpoint, Map<String, dynamic> queryParameters) {
+    var query = {'appid': apiKey};
+    if (queryParameters != null) {
+      query = query..addAll(queryParameters);
+    }
+
+    var uri = Uri(
+      scheme: 'http',
+      host: 'api.openweathermap.org',
+      path: 'data/2.5/$endpoint',
+      queryParameters: query,
+    );
+
+    print('fetching $uri');
+
+    return uri;
+  }
+
   Future<String> getCityNameFromLocation(
       {double latitude, double longitude}) async {
-    final url =
-        '$baseUrl/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey';
-    print('fetching $url');
+    final uri = _buildUri('weather', {
+      'lat': latitude.toString(),
+      'lon': longitude.toString(),
+    });
 
-    final res = await this.httpClient.get(Uri.parse(url));
+    final res = await this.httpClient.get(uri);
+
     if (res.statusCode != 200) {
       throw HTTPException(res.statusCode, "unable to fetch weather data");
     }
+
     final weatherJson = json.decode(res.body);
     return weatherJson['name'];
   }
 
   Future<Weather> getWeatherData(String cityName) async {
-    final url = '$baseUrl/data/2.5/weather?q=$cityName&appid=$apiKey';
-    print('fetching $url');
-    final res = await this.httpClient.get(Uri.parse(url));
+    final uri = _buildUri('weather', {'q': cityName});
+
+    final res = await this.httpClient.get(uri);
 
     if (res.statusCode != 200) {
       throw HTTPException(res.statusCode, "unable to fetch weather data");
     }
+
     final weatherJson = json.decode(res.body);
     return Weather.fromJson(weatherJson);
   }
 
   Future<List<Weather>> getForecast(String cityName) async {
-    final url = '$baseUrl/data/2.5/forecast?q=$cityName&appid=$apiKey';
-    print('fetching $url');
-    final res = await this.httpClient.get(Uri.parse(url));
+    final uri = _buildUri('forecast', {'q': cityName});
+
+    final res = await this.httpClient.get(uri);
+
     if (res.statusCode != 200) {
       throw HTTPException(res.statusCode, "unable to fetch weather data");
     }
+
     final forecastJson = json.decode(res.body);
     List<Weather> weathers = Weather.fromForecastJson(forecastJson);
     return weathers;
